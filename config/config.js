91ec23c3837db1ac552d54b53f59ffc7ec1ec423
Sa Eun Soo <tkdmstn0304@naver.com>
2020-11-21 03:07:02 +0900

    setting

--- config/config.js
+++ config/config.js
@@ -1,12 +1,7 @@
 module.exports = {
-  // host: "localhost",
-  // port: "3306",
-  // user: "root",
-  // password: "root",
-  // database: "molab",
-  host: 'localhost',
-  user: 'root',
-  password: 'euna',
-  database: 'MOLAB',
-  dateStrings: 'date',
+  host: "us-cdbr-east-02.cleardb.com",
+  port: "3306",
+  user: "b5ea85694c788f",
+  password: "b9bb6744",
+  database: "heroku_cef972af551034b",
 };

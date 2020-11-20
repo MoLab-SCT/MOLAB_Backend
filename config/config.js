e32e840544a36a145d8cc03512760dec78941194
Rory0304 <42709747+Rory0304@users.noreply.github.com>
2020-11-21 04:37:57 +0900

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,7 +1,2 @@
-module.exports = {
-  host: "us-cdbr-east-02.cleardb.com",
-  port: "3306",
-  user: "b5ea85694c788f",
-  password: "b9bb6744",
-  database: "heroku_cef972af551034b",
+module.exports ={
 };

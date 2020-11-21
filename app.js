1ab42b1962960ef9637978a5735ef623c5909dbb
Sa Eun Soo <tkdmstn0304@naver.com>
2020-11-21 14:02:58 +0900

    solve cors policy

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ var signup = require("./routes/signup");
 var auth = require("./routes/auth");
 var app = express();
 var corsOptions = {
-  origin: "http://localhost:3001", // 허용되는 Origin
+  origin: "https://5fb8166fceb3710c77c2ee7a--molab.netlify.app", // 허용되는 Origin
   credentials: true,
   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
